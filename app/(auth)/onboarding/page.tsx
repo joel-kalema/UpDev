84b66323ad5ed7930cf0b358a1b264fbb91d75d0
joel-kalema <joelkalema63@gmail.com>
2023-08-28 21:20:05 +0200

    update comps

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,15 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+
 async function Page() {
     return (
-        <main>
-            <h1 className="head-text">hello</h1>
+        <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
+            <h1 className='head-text'>Onboarding</h1>
+            <p className='mt-3 text-base-regular text-light-2'>
+                Complete your profile now, to use Threds.
+            </p>
+            <section className='mt-9 bg-dark-2 p-10'>
+                <AccountProfile />
+            </section>
         </main>
     )
 }
---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const AccountProfile = () => {
+    return(
+        <div>
+            acount profile
+        </div>
+    )
+}
+
+export default AccountProfile;
