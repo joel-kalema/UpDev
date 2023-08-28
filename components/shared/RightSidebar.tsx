42a4f6a704b712ce07d1e32b131801dc779a0cf9
joel-kalema <joelkalema63@gmail.com>
2023-08-28 19:53:59 +0200

    update RightSidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,8 +1,12 @@
 function RightSidebar() {
-    return(
-        <>
-        <h1>RightSidebar</h1>
-        </>
+    return (
+        <section className='custom-scrollbar rightsidebar'>
+            <div className='flex flex-1 flex-col justify-start'>
+                <h3 className='text-heading4-medium text-light-1'>
+                    Suggested Communities
+                </h3>
+            </div>
+        </section>
     )
 }
 
