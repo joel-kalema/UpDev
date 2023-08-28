a19b72dfc7fa1199d20de2e1f9ace01995d47b5b
joel-kalema <joelkalema63@gmail.com>
2023-08-28 20:24:51 +0200

    update compos

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
+// import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
+      {/* <UserButton afterSignOutUrl="/"/> */}
       <h1 className="head-text text-left">Home</h1>
     </div>
   )
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -6,6 +6,11 @@ function RightSidebar() {
                     Suggested Communities
                 </h3>
             </div>
+
+            <div className='flex flex-1 flex-col justify-start'>
+                <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
+            </div>
+
         </section>
     )
 }
