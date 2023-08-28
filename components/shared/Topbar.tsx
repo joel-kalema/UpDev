dcc93d9e5e146236cb1e79d2c1397749344b7586
joel-kalema <joelkalema63@gmail.com>
2023-08-28 14:57:06 +0200

    finalize the topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,9 +1,41 @@
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
 function Topbar() {
-    return(
-        <>
-        <h1>Topbar</h1>
-        </>
-    )
+    return (
+        <nav className='topbar'>
+            <Link href='/' className='flex items-center gap-4'>
+                <Image src='/logo.svg' alt='logo' width={28} height={28} />
+                <p className='text-heading3-bold text-light-1 max-xs:hidden'>Updev</p>
+            </Link>
+
+            <div className='flex items-center gap-1'>
+                <div className='block md:hidden'>
+                    <SignedIn>
+                        <SignOutButton>
+                            <div className='flex cursor-pointer'>
+                                <Image
+                                    src='/assets/logout.svg'
+                                    alt='logout'
+                                    width={24}
+                                    height={24}
+                                />
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                </div>
+
+                <OrganizationSwitcher
+                    appearance={{
+                        elements: {
+                            organizationSwitcherTrigger: "py-2 px-4",
+                        },
+                    }}
+                />
+            </div>
+        </nav>
+    );
 }
 
 export default Topbar;
