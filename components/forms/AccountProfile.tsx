adf5be0884f86c3958f6cb2acd824c416c72f687
joel-kalema <joelkalema63@gmail.com>
2023-08-30 00:36:58 +0200

    implemated the upload image

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,7 +20,7 @@ import * as z from "zod"
 
 
 import { UserValidation } from "@/lib/validations/user";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 
 interface Props {
     user: {
@@ -35,6 +35,8 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+
+    const [files, setFiles] = useState<File[]>([]);
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -52,9 +54,28 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         console.log(values)
     }
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (
+        e: ChangeEvent<HTMLInputElement>,
+        fieldChange: (value: string) => void
+    ) => {
         e.preventDefault();
-    }
+
+        const fileReader = new FileReader();
+
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+            setFiles(Array.from(e.target.files));
+
+            if (!file.type.includes("image")) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || "";
+                fieldChange(imageDataUrl);
+            };
+
+            fileReader.readAsDataURL(file);
+        }
+    };
 
     return (
         <div>
