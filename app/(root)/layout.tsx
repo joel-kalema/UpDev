3e52f69e56dbf3d94f722408ae6f08ae909e81cb
joel kalema <joelkalema63@gmail.com>
2024-07-22 01:51:37 +0200

    added upadated the onboarding funtionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,27 +1,34 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Topbar from '@/components/shared/Topbar'
-import LeftSidebar from '@/components/shared/LeftSidebar'
-import RightSidebar from '@/components/shared/RightSidebar'
-import Bottombar from '@/components/shared/Bottombar'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-const inter = Inter({ subsets: ['latin'] })
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Updev',
-  description: 'Let us twite',
-}
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
         <body className={inter.className}>
           <Topbar />
 
@@ -30,6 +37,7 @@ export default function RootLayout({
             <section className='main-container'>
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
 
@@ -37,5 +45,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
