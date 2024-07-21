048a62952392fa397ab99887afe9ab5c91765040
joel kalema <joelkalema63@gmail.com>
2024-07-21 16:43:26 +0200

    added community page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,8 @@ import { connectToDB } from "../mongoose";
 import User from "../models/user.model";
 import { revalidatePath } from "next/cache";
 import { SortOrder } from "mongoose";
+import Community from "../models/community.model";
+import Thread from "../models/thread.model";
 
 interface Params {
   userId: string;
@@ -12,6 +14,38 @@ interface Params {
   bio: string;
   image: string;
   path: string;
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        {
+          path: "community",
+          model: Community,
+          select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+        },
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id", // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+    return threads;
+  } catch (error) {
+    console.error("Error fetching user threads:", error);
+    throw error;
+  }
 }
 
 export async function updateUser({
@@ -61,3 +95,33 @@ export async function fetchUser(userId: string) {
     throw error;
   }
 }
+
+
+// export async function getActivity(userId: string) {
+//   try {
+//     connectToDB();
+
+//     // Find all threads created by the user
+//     const userThreads = await Thread.find({ author: userId });
+
+//     // Collect all the child thread ids (replies) from the 'children' field of each user thread
+//     const childThreadIds = userThreads.reduce((acc, userThread) => {
+//       return acc.concat(userThread.children);
+//     }, []);
+
+//     // Find and return the child threads (replies) excluding the ones created by the same user
+//     const replies = await Thread.find({
+//       _id: { $in: childThreadIds },
+//       author: { $ne: userId }, // Exclude threads authored by the same user
+//     }).populate({
+//       path: "author",
+//       model: User,
+//       select: "name image _id",
+//     });
+
+//     return replies;
+//   } catch (error) {
+//     console.error("Error fetching replies: ", error);
+//     throw error;
+//   }
+// }
