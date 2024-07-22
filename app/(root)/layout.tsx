46130a8dc6331924ddb0c3dd667dcc25120e5ffc
joel kalema <joelkalema63@gmail.com>
2024-07-22 16:27:02 +0200

    updat the project description

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,8 +13,8 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads application",
+  title: "UpDev",
+  description: "Help Developers to tweet about new technologies",
 };
 
 export default function RootLayout({
