b2266e818c7b768b3bf6cec76916199a5b6e9412
joel kalema <joelkalema63@gmail.com>
2024-08-09 20:03:18 +0200

    fix design

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -21,7 +21,7 @@ function ProfileHeader({
   type,
 }: Props) {
   return (
-    <div className='flex w-full flex-col justify-start'>
+    <div className='flex w-full flex-col justify-start px-6'>
       <div className='flex items-center justify-between'>
         <div className='flex items-center gap-3'>
           <div className='relative h-20 w-20 object-cover'>
